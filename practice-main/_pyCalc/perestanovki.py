22d37513670dcbb26cc684cfcbf196fc60b4087c
mlavdeeva <87315042+mlavdeeva@users.noreply.github.com>
2021-07-22 03:40:14 +0300

    1

--- practice-main/_pyCalc/perestanovki.py
+++
@@ -1,6 +0,0 @@
-import math
-
-
-class P:
-    n = int(input("number = "))
-    print(math.factorial(n))
--- practice-main/ui_py.py
+++
@@ -1,8 +0,0 @@
-"""
-Конвертация .ui-файла в .py-файл
-"""
-
-import subprocess
-
-subprocess.run(["pyuic5", "calculatorDialog.ui", "-o", "calculatorDialog.py"])
-
